2cdb52fbea5696830ea59f6704c73af2e95ad605
anokata <anokata8@mail.ru>
2021-08-27 14:28:32 +0300

    [etd] [chg] vue fragmentate.

---
+++ vuemodapp/src/app.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Dude!, by vue.js module.vue'
+    }
+  }
+}
--- vuemodapp/src/app.vue
+++ vuemodapp/src/app.vue
@@ -1,23 +1,3 @@
-<template>
-  <div id="test">
-    <h2>{{msg}}</h2>
-  </div>
-</template>
- 
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Test Due by vue.js module.vue'
-    }
-  }
-}
-</script>
- 
-<style>
-#test {
-  font-family: Verdana;
-  color: navy;
-}
-</style>
+<template src="./app.html"></template>
+<style src="./app.css"></style>
+<script src="./app.js"></script>
