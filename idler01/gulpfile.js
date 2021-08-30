e6e3e730d8b7b8625f9f681cd7d874aec3adba86
anokata <anokata8@mail.ru>
2021-08-30 10:48:06 +0300

    [add] gulpfile. [etd] webpack build index->main

---
+++ idler01/gulpfile.js
@@ -0,0 +1,15 @@
+const { series, parallel } = require('gulp');
+
+function defaultTask(cb) {
+    cb();
+    console.log(arguments.callee.name);
+}
+
+function one(c){ console.log("one"); c();}
+function two(c){ console.log(arguments.callee.name); c(); }
+function thr(c){ console.log(arguments.callee.name); c(); }
+
+exports.default = defaultTask;
+exports.build = defaultTask;
+exports.ott = series(one, two, thr);
+exports.par = parallel(one, two, thr);
