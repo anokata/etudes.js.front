3296437c633490f3ffb73a8915395c88ee5e7281
anokata <anokata8@mail.ru>
2021-09-08 17:21:42 +0300

    [etd] primise, async await syntax

---
+++ app1/nodeapp3.js
@@ -0,0 +1,41 @@
+const colors = require('colors');
+const l = console.log;
+
+// async == return Promise.resolve
+async function asyncF1() { return 74; }
+l(asyncF1());  // Promise
+asyncF1().then((val) => l(`asyncF1 complete with ${val}`.blue));
+
+async function asyncF2() {
+    l("[F2] start F2");
+    let p1 = new Promise((resolve, reject) => {
+        setTimeout(()=> resolve("timeout: done F2"), 500);
+    });
+    let result = await p1;
+    l("[F2] after await, in F2, result:".green, result);
+    l("[F2] end F2");
+}
+
+l("[G] before F2");
+asyncF2();
+l("[G] after F2");
+
+l("anon async");
+
+(async () =>{
+    await new Promise((r)=> setTimeout(()=>r(), 200));
+    l("anon in".yellow);
+})();
+
+l("[G] end. PromiseTime!".red);
+
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  return 10;
+}
+
+function f() {
+    wait().then((r) => l(r));
+}
+
+f();
--- app1/testapp2.js
+++ app1/testapp2.js
@@ -1,8 +1,10 @@
 /* import 'colors'; */
 let l = console.log;
 let a = 8;
-a = "c".codePointAt(0);
-l(a);
+l(String.prototype.codePointAt);
+let _a = "c".codePointAt(0);
+/* a = _a instanceof number ? _a : 0; */
+l(a, _a);
 function logtype(value) {
     l(`|${typeof value}|=${value}`);
 }
@@ -50,3 +52,50 @@ paramsopt(100);
 paramsopt(100, 200);
 paramsinit(100);
 paramsinit(100, 200);
+l("*** function type");
+let f1;
+f1 = l;
+let f2;
+f2 = logtype;
+function usefunc(value, func) {
+    return func(value.toUpperCase());
+}
+function f1s(s) {
+    return `[${s.trim()}]`;
+}
+l(usefunc("   abc!=144   ", f1s));
+let f3;
+f3 = (v) => 3;
+l(f3(0), f3);
+f3 = (v) => v * 3;
+l(f3(23));
+let p1;
+let a4 = [1];
+let a5 = new Array(1);
+l(typeof (a4), typeof (a5));
+l();
+l("*** Union");
+let u1;
+u1 = 1;
+u1 = "a";
+function uf1(a) { }
+let id = 8;
+function pSz(a) { l(a); }
+;
+pSz();
+l();
+l("*** obj type");
+let user1 = { name: "Jonh", prof: "cleaner", cost: 1e3 };
+l(user1, typeof (user1));
+user1 = { name: "Jane" };
+function objfunc(o) { return { o: o }; }
+;
+l(objfunc({ x: 'y' }));
+l("prop IN obj");
+function testObj(obj) { return "ok" in obj; }
+l(testObj({ ok: true }));
+l("decompose obj");
+function showObj({ value, x = "_", y = "Y" }) {
+    console.log(`Obj: {${value}-${x}-${y}}`);
+}
+showObj({ value: 512 });
