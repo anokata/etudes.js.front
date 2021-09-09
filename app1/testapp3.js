76603da66c5e972e0fad1543c839c342ae85cc8d
anokata <anokata8@mail.ru>
2021-09-09 17:29:17 +0300

    [ts] arrays

---
+++ app1/testapp3.js
@@ -0,0 +1,13 @@
+const log = console.log;
+log("==start==");
+let prices = [0.1, 0.02, 0.93];
+log(prices);
+let doublePrices = Array.from("127578954892").map((v) => (+v) * 10);
+log(doublePrices);
+doublePrices = doublePrices.map((v) => v * 2.02);
+log(doublePrices);
+let staticPrices = doublePrices.map((v) => +(v / 34).toFixed(2));
+log(staticPrices.join(" "));
+const [head, ...tail] = staticPrices;
+log(head);
+log(tail);
---
+++ app1/testapp3.ts
@@ -0,0 +1,20 @@
+const log:(...a)=>void =console.log;
+log("==start==");
+type Numbers = number[];
+let prices: Numbers = [0.1,0.02,0.93];
+log(prices);
+
+let doublePrices: Array<number> = Array.from("127578954892").map((v)=>(+v)*10);
+log(doublePrices);
+doublePrices = doublePrices.map((v)=>v*2.02);
+log(doublePrices);
+
+let staticPrices: readonly number[] = doublePrices.map((v) => +(v/34).toFixed(2));
+log(staticPrices.join(" "));
+
+const [head, ...tail] = staticPrices;
+log(head);
+log();log("Tupes...");
+
+
+
