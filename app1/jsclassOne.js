9926de9c55a5d2f328710e677975957c168f55a0
anokata <anokata8@mail.ru>
2021-09-09 19:16:54 +0300

    [etd] js class, extend, get, set, super.

--- app1/jsclassOne.js
+++ app1/jsclassOne.js
@@ -1,7 +1,8 @@
 const log = console.log;
+log("*** Classes");
 class Lamp {
-    constructor() {
-        this.model = "basic";
+    constructor(model) {
+        this.model = model ?? "basic";
     }
 
     describe() {
@@ -12,3 +13,57 @@ class Lamp {
 log(Lamp, typeof Lamp);
 let basicLamp = new Lamp();
 log(basicLamp, typeof basicLamp, basicLamp.describe());
+log(Lamp.prototype.describe);
+log(Object.getOwnPropertyNames(Lamp.prototype));
+
+log("*** Class exp");
+
+const myClass = class {
+    do() {
+        log("(unnamed class)");
+    }
+};
+new myClass().do();
+
+function ClassFactory(param) {
+    return class {
+        constructor() {
+            this.x = param;
+        }
+        do() {
+            console.log(`x: ${++this.x}`);
+        }
+    };
+}
+
+let ArtificalClass = ClassFactory(80);
+let artificalClassObj = new ArtificalClass();
+artificalClassObj.do();
+
+log();
+
+class BigLamp extends Lamp {
+    unit = 'l';
+
+    constructor(size) {
+        super('B.G.0');
+        this._size = 0;
+        this.size = size;
+    }
+    get size() {
+        return `${this._size}${this.unit}`;
+    }
+    set size(value) {
+        if ((typeof value == 'number') && (value > 10))
+            this._size = value;
+    }
+    toString() {
+        return `BigLamp: ${this.size} model(${this.model})`;
+    }
+    describe() {
+        return `Big${super.describe()}.`;
+    }
+}
+
+let blamp = new BigLamp(18);
+log(`${blamp} || ${blamp.describe()}`);
