a0f26a633561c5dfb138c6e744928bae00978dc0
anokata <anokata8@mail.ru>
2021-09-12 14:44:23 +0300

    [nodejs] express.js. middleware use

--- app1/expressServ.js
+++ app1/expressServ.js
@@ -1,13 +1,31 @@
 const log = console.log;
 const express = require("express");
 const server = express();
-log(server);
+// log(server);
 
+// middlewares
+server.use((req, res, next) => {
+    log("Middleware #1 ... Preparing");
+    next();
+});
+
+server.use("/about", (req, res, next) => {
+    log("Middleware #about");
+    next();
+});
+
+// main roots
 server.get("/", root);
+server.get("/about", (req, res) => {
+    log("/about get")
+    res.send("About");
+});
 
 function root(req, res) {
-    res.send("<h3>Hi Express.js</h3>");
+    res.send("<h3>Hi Express.js</h3> <a href='/about' >a</a>");
 }
+
+
 
 server.listen(3000);
 log("listen on 3000 port");
