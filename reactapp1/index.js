52caf9ed4c802249bd203edd508dd9886d1c8065
anokata <anokata8@mail.ru>
2021-09-12 09:30:02 +0300

    [react] func components, rerender jsx.

--- reactapp1/index.js
+++ reactapp1/index.js
@@ -1,18 +1,39 @@
+const log = console.log;
 const a = Array.from(new Array(10).keys());
 const b = a.join("-");
 const divStyle = {
   color: '#abc'
 };
+let element1 = /*#__PURE__*/React.createElement("b", null, "hi");
+log(element1);
 
 class Bitton extends React.Component {
   render() {
     return /*#__PURE__*/React.createElement("button", null, "OK");
   }
 
+} // functional component
+
+
+function Timer() {
+  let ms = new Date().getMilliseconds();
+  let sec = new Date().getSeconds();
+  return /*#__PURE__*/React.createElement("span", null, sec, ":", ms);
 }
 
+function renderTimer() {
+  ReactDOM.render( /*#__PURE__*/React.createElement(Timer, null), document.getElementById("timer"));
+}
+
+setInterval(renderTimer, 10);
+
+function tick() {
+  ReactDOM.render( /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h3", null, new Date().toLocaleTimeString(), "."), "ms:", /*#__PURE__*/React.createElement(Timer, null)), document.getElementById("header"));
+}
+
+setInterval(tick, 1000);
 ReactDOM.render( /*#__PURE__*/React.createElement("div", {
   className: "dot block-a",
   style: divStyle
-}, /*#__PURE__*/React.createElement("h1", null, "Hello React", b), /*#__PURE__*/React.createElement("p", null, a.map(x => String.fromCodePoint(x + 100)))), document.getElementById("app"));
+}, /*#__PURE__*/React.createElement("h1", null, "Hello React", b, "ms:", /*#__PURE__*/React.createElement(Timer, null)), /*#__PURE__*/React.createElement("p", null, a.map(x => String.fromCodePoint(x + 100)))), document.getElementById("app"));
 ReactDOM.render( /*#__PURE__*/React.createElement(Bitton, null), document.getElementById("app2"));
--- reactapp1/index.jsx
+++ reactapp1/index.jsx
@@ -1,3 +1,4 @@
+const log = console.log;
 
 const a = Array.from(new Array(10).keys());
 const b = a.join("-");
@@ -5,16 +6,46 @@ const divStyle = {
     color: '#abc',
 };
 
+let element1 = <b>hi</b>;
+log(element1);
+
 class Bitton extends React.Component {
 render() {
 return <button>OK</button>;
 }
 }
 
+// functional component
+function Timer(){
+    let ms = new Date().getMilliseconds();
+    let sec = new Date().getSeconds();
+    return <span>{sec}:{ms}</span>;
+}
+
+function renderTimer() {
+    ReactDOM.render(
+        <Timer/>,
+        document.getElementById("timer")
+    );
+}
+setInterval(renderTimer, 10);
+
+function tick() {
+    ReactDOM.render(
+    <div>
+    <h3>{new Date().toLocaleTimeString()}.</h3>
+    ms:<Timer/>
+    </div>,
+    document.getElementById("header")
+    );
+}
+setInterval(tick, 1000);
+
 ReactDOM.render(
 <div className="dot block-a" style={divStyle}>
 <h1>Hello React 
 {b}
+    ms:<Timer/>
 </h1>
 <p>{a.map(x=>String.fromCodePoint(x+100))}</p>
 </div>,
