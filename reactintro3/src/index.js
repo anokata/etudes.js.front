9012c824947e73a080ed76a5b6898382c763c8b2
anokata <anokata8@mail.ru>
2021-10-04 12:42:16 +0300

    [css] styling a little

--- reactintro3/src/index.js
+++ reactintro3/src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={()=> {this.props.onClick();}}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button 
+      className="square" 
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -22,6 +19,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
    
   }
@@ -36,11 +34,15 @@ class Board extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice(); // copy
     squares[i] = 'X';
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ 
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
