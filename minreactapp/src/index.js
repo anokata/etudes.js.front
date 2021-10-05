b7ab6ecae13851cc2ae17e88cd3bc45ebddc1e8d
anokata <anokata8@mail.ru>
2021-10-05 12:52:18 +0300

    [add] minimal react app, self raised

---
+++ minreactapp/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log(React);
+console.log(ReactDOM);
+
+ReactDOM.render(
+  <h1>good day webpack with react by babel!</h1>,
+  document.getElementById("root")
+);
