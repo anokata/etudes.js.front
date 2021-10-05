253aa3fce95e6fe6711ccdcfba049a8d5acdae78
anokata <anokata8@mail.ru>
2021-10-05 15:21:06 +0300

    [add] component composition - add list with elems

--- reactdoc4/src/components.js
+++ reactdoc4/src/components.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 console.log("hi components");
 
@@ -13,7 +13,25 @@ export class ButtonCu extends React.Component {
   }
 }
 
-const root = <main>c<ButtonHu text="hu"/><ButtonCu value="CU"/></main>;
+export function ListElem(props) {
+  return <li>{props.text}</li>;
+}
+
+export function UnsortedList(props) {
+  console.log(props.children);
+  const elems = Array(props.length | 1).fill(null).map((e, i) => {
+    return <ListElem key={i} text={`Elem#${i + 1}`} />;
+  });
+  return <ul>{elems}</ul>;
+}
+
+const root = (
+  <main>
+    c<ButtonHu text="hu" />
+    <ButtonCu value="CU" />
+    <div><UnsortedList length="3" >text<span>in</span> </UnsortedList></div>
+  </main>
+);
 
 ReactDOM.render(root, document.getElementById("component"));
 
