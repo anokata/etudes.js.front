836c754741368f32ac67424749cad343fa76aa8a
anokata <anokata8@mail.ru>
2021-10-06 14:58:20 +0300

    [hooks] [redux-toolkit]

--- reactdoc4/src/hooks.js
+++ reactdoc4/src/hooks.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-
 
 function State1() {
   const [count, setCount] = useState(0);
+  console.log(count, typeof(count));
   return (
     <div>
       <p>clicked {count} times</p>
--- reactdoc4/src/statereduxtest.js
+++ reactdoc4/src/statereduxtest.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+import { useSelector, useDispatch } from 'react-redux'; // hooks for redux
+import { configureStore } from '@reduxjs/toolkit'
+export default configureStore({  reducer: {},})
+
+
 function incrementReducer(state = {value: 0}, action) {
   if (action.type === "inc") return {value:state.value + 1};
   return state;
