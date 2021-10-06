01aac7707cb7b73fad71134df73db6b2f9fce49c
anokata <anokata8@mail.ru>
2021-10-06 15:07:32 +0300

    [redux-react] counter tutor.

--- reactdoc4/src/statereduxtest.js
+++ reactdoc4/src/statereduxtest.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { useSelector, useDispatch } from "react-redux"; // hooks for redux
+import { configureStore } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+// ------------------------------------------
 
-import { useSelector, useDispatch } from 'react-redux'; // hooks for redux
-import { configureStore } from '@reduxjs/toolkit'
-export default configureStore({  reducer: {},})
-
-
-function incrementReducer(state = {value: 0}, action) {
-  if (action.type === "inc") return {value:state.value + 1};
+function incrementReducer(state = { value: 0 }, action) {
+  if (action.type === "inc") return { value: state.value + 1 };
   return state;
 }
 
-let store = createStore(incrementReducer);
+const initialState = { value: 0 };
 
-store.subscribe(() => {console.log(`store: ${store.getState().value}`)});
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+let log = console.log;
+log(counterSlice);
+log(counterSlice.reducer);
+log(counterSlice.actions);
+log(counterSlice.actions.increment);
 
-store.dispatch({type:"inc"});
+const store0 = configureStore({ reducer: {
+  counter: counterReducer,
+} }); //toolkit style
 
+let store = createStore(incrementReducer); // basic style
+
+store.subscribe(() => {
+  console.log(`store: ${store.getState().value}`);
+});
+store.dispatch({ type: "inc" });
+
+// ------------------------------------------
 const root = document.getElementById("state");
-const elem = <Provider store={store}><main>HHH</main></Provider>
-
+const elem = (
+  <Provider store={store}>
+    <main>HHH</main>
+  </Provider>
+);
 if (root) ReactDOM.render(elem, root);
