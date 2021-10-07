71605aee7a075cb46f602df599b414dea414e670
anokata <anokata8@mail.ru>
2021-10-07 12:23:46 +0300

    [creating] click field

--- idler02react/src/index.js
+++ idler02react/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
-console.log(React);
-console.log(ReactDOM);
+function ClickField(props) {
+  const [clicker, setClick] = useState({
+    clicks: 0,
+    incrementBy: 0.1,
+  });
 
-ReactDOM.render(
-  <h1>Idler 02</h1>,
-  document.getElementById("root")
-);
+  console.log("render cli<C-BS>");
+  return (
+    <div className="click-field"></div>
+  );
+}
+
+let root = (<main>
+<h1>Idler 02</h1>
+  <ClickField />
+  </main>);
+
+ReactDOM.render( root, document.getElementById("root"));
