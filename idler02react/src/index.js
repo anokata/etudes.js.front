d4736b89530efa6455d6546203d58ef159673462
anokata <anokata8@mail.ru>
2021-10-07 17:58:22 +0300

    [add] redux init state, create store, make action maker, define reducer for click action and dispatch on click it.

--- idler02react/src/index.js
+++ idler02react/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { useState, useEffect } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useState, useEffect } from "react";
+import { configureStore } from "@reduxjs/toolkit";
 
 function ClickField(props) {
   const [clicker, setClick] = useState({
@@ -11,19 +12,50 @@ function ClickField(props) {
   console.log(`render cli<C-BS> ${clicker.clicks} ${clicker.incrementBy}`);
   useEffect(() => {
     console.log(`effect`);
+    console.log("state: ", store.getState());
   });
   return (
-    <div className="click-field" onClick={() => updateClick(setClick, clicker)}>Clicks: {clicker.clicks}</div>
+    <div className="click-field" onClick={() => updateClick(setClick, clicker)}>
+      Clicks: {clicker.clicks}
+    </div>
   );
 }
 
 function updateClick(setClick, clicker) {
-  setClick({...clicker, clicks: clicker.clicks + clicker.incrementBy});
+  setClick({ ...clicker, clicks: clicker.clicks + clicker.incrementBy });
+  store.dispatch(clickAction());
 }
 
-let root = (<main>
-<h1>Idler 02</h1>
-  <ClickField />
-  </main>);
+// redux
+const initialState = {
+  clicks: 0,
+};
+const store = configureStore({ reducer: rootReducer });
 
-ReactDOM.render( root, document.getElementById("root"));
+function rootReducer(state = initialState, action) {
+  if (action.type === CLICK_ACTION) {
+    let newState = {...state};
+    newState.clicks += 1;
+    return newState;
+  }
+  return state;
+}
+const CLICK_ACTION = "click/clicked";
+// make an action
+function clickAction() {
+  return { 
+    type: CLICK_ACTION, 
+    payload: 0 
+  };
+}
+// store.dispatch(clickAction());
+// -----
+
+let root = (
+  <main>
+    <h1>Idler 02</h1>
+    <ClickField />
+  </main>
+);
+
+ReactDOM.render(root, document.getElementById("root"));
