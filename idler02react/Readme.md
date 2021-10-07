80136b38b452b3b7b9788d3061cc48f034bb11c1
anokata <anokata8@mail.ru>
2021-10-07 11:33:56 +0300

    [start] idler 02 on react/redux

---
+++ idler02react/Readme.md
@@ -0,0 +1,10 @@
+idler on react/redux
+
+install:
+npm install
+
+dev run:
+npm run dev
+
+build:
+npm run build
---
+++ idler02react/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log(React);
+console.log(ReactDOM);
+
+ReactDOM.render(
+  <h1>Idler 02</h1>,
+  document.getElementById("root")
+);
