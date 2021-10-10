c88cb7a9a2186047fa104a8358615b74c9391fac
anokata <anokata8@mail.ru>
2021-10-10 19:19:26 +0300

    [pretty]

--- todolist/src/app/todolist.js
+++ todolist/src/app/todolist.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 //state
 
-function createData(name, text) {
-  return { name, text};
+export default function TodoList(props) {
+  return (
+    <div>
+      <InputArea />
+      <TodoTable />
+    </div>
+  );
 }
 
-const rows = [
-  createData("-", "do something"),
-];
+function createData(name, text) {
+  return { name, text };
+}
 
+const rows = [createData("-", "do something")];
 
 function InputArea(props) {
   return (
@@ -45,7 +51,7 @@ function TodoTable(props) {
           {rows.map((row) => (
             <TableRow
               key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {row.name}
@@ -57,11 +63,4 @@ function TodoTable(props) {
       </Table>
     </TableContainer>
   );
-}
-
-export default function TodoList(props) {
-  return (<div>
-    <InputArea />
-    <TodoTable />
-  </div>);
 }
