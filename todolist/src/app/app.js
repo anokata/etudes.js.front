8da4f35da4bb753629f55a18a8ec005cd5fff011
anokata <anokata8@mail.ru>
2021-10-10 18:26:20 +0300

    [start] todolist app in reactjs

---
+++ todolist/src/app/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//state
+//todolist
+
+export default function App(props) {
+  return <h2>app</h2>;
+}
+
---
+++ todolist/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path:path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, ".", "index.html"),
+    }),
+  ],
+   performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+  },
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname, ''),
+      },
+      {
+        directory: path.join(__dirname, 'dist'),
+      },
+      {
+        directory: path.join(__dirname, 'css'),
+      },
+    ],
+  },
+}
