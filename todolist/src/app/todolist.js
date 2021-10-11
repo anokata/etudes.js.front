2708baa32e378f81feea3d821e6177958f340c2b
anokata <anokata8@mail.ru>
2021-10-11 11:59:45 +0300

    [pretty] status toggle click also work on type

--- todolist/src/app/todolist.js
+++ todolist/src/app/todolist.js
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -9,17 +9,23 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
-import { useSelector, useDispatch } from 'react-redux';
-import { store, selectList, addTodo, delTodo, toggleDoneStatus, makeTodoRec } from './store';
-import { connect } from 'react-redux';
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  store,
+  selectList,
+  addTodo,
+  delTodo,
+  toggleDoneStatus,
+  makeTodoRec,
+} from "./store";
+import { connect } from "react-redux";
 /* TODO
 Переключение статуса записи Выполнено / Невыполненный при клике на её название;
 Фильтрация по типу (все, выполненные, невыполненные);
 Отображение счётчика выполненных и невыполненных задач;
  * */
-
 
 export default function TodoList(props) {
   return (
@@ -40,7 +46,7 @@ class InputArea extends React.Component {
     this.state = {
       value: "",
       alert: null,
-    }
+    };
   }
 
   handleChange(e) {
@@ -48,7 +54,6 @@ class InputArea extends React.Component {
       value: e.target.value,
     });
   }
-
 
   addTodoItem() {
     if (this.state.value.length > InputArea.TODO_MAX_LENGTH) {
@@ -62,44 +67,54 @@ class InputArea extends React.Component {
 
   showAlert(text) {
     this.setState({
-      alert:  <TodoAlert text={text} />,
-    })
-    setTimeout(()=>this.setState({alert: null}), 1500);
+      alert: <TodoAlert text={text} />,
+    });
+    setTimeout(() => this.setState({ alert: null }), 1500);
   }
 
   render() {
     return (
       <div className="todo-input-area">
         <div className="todo-input">
-          <TextField id="outlined-basic" label="Todo text" variant="outlined" onChange={(e) => this.handleChange(e)}/>
+          <TextField
+            id="outlined-basic"
+            label="Todo text"
+            variant="outlined"
+            onChange={(e) => this.handleChange(e)}
+          />
         </div>
-        <Button variant="contained" onClick={this.addTodoItem}>add</Button>
-        <div className="alert">
-          {this.state.alert}
-        </div>
+        <Button variant="contained" onClick={this.addTodoItem}>
+          add
+        </Button>
+        <div className="alert">{this.state.alert}</div>
       </div>
     );
   }
 }
 const mapDispatchToProps = (dispatch) => {
-    return {
-        addTodo: (data) => {
-          dispatch(addTodo(data));
-        },
-        delTodo: (id) => {
-          dispatch(delTodo(id));
-        },
-        toggleDoneStatus: (id) => {
-          dispatch(toggleDoneStatus(id));
-        },
-    }
+  return {
+    addTodo: (data) => {
+      dispatch(addTodo(data));
+    },
+    delTodo: (id) => {
+      dispatch(delTodo(id));
+    },
+    toggleDoneStatus: (id) => {
+      dispatch(toggleDoneStatus(id));
+    },
+  };
 };
 const InputAreaD = connect(null, mapDispatchToProps)(InputArea);
 
 class DelTodoButtonPlain extends React.Component {
   render() {
     return (
-        <Button variant="contained" onClick={() => this.props.delTodo(this.props.id)}>X</Button>
+      <Button
+        variant="contained"
+        onClick={() => this.props.delTodo(this.props.id)}
+      >
+        X
+      </Button>
     );
   }
 }
@@ -109,13 +124,13 @@ function TodoTable(props) {
   const dispatch = useDispatch();
   const todolist = useSelector(selectList);
   const tableHead = (
-        <TableHead>
-          <TableRow>
-            <TableCell>Delete</TableCell>
-            <TableCell>Type</TableCell>
-            <TableCell>Text</TableCell>
-          </TableRow>
-        </TableHead>
+    <TableHead>
+      <TableRow>
+        <TableCell>Delete</TableCell>
+        <TableCell>Type</TableCell>
+        <TableCell>Text</TableCell>
+      </TableRow>
+    </TableHead>
   );
   const handleClick = (e, id) => {
     dispatch(toggleDoneStatus(id));
@@ -124,7 +139,7 @@ function TodoTable(props) {
     if (type === "Done") return "type-cell-done";
     if (type === "Undone") return "type-cell-undone";
     return "";
-  }
+  };
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -138,10 +153,18 @@ function TodoTable(props) {
               <TableCell component="th" scope="row">
                 <DelTodoButton id={row.id} />
               </TableCell>
-              <TableCell component="th" scope="row" className={`type-cell ${typeClass(row.type)}`}>
+              <TableCell
+                component="th"
+                scope="row"
+                className={`type-cell ${typeClass(row.type)}`}
+                onClick={(e) => handleClick(e, row.id)}
+              >
                 {row.type}
               </TableCell>
-              <TableCell className="todo-text" onClick={(e) => handleClick(e, row.id)}>
+              <TableCell
+                className="todo-text"
+                onClick={(e) => handleClick(e, row.id)}
+              >
                 {row.text}
               </TableCell>
             </TableRow>
@@ -154,7 +177,7 @@ function TodoTable(props) {
 
 function TodoAlert(props) {
   return (
-    <Stack sx={{ width: '100%' }} spacing={2}>
+    <Stack sx={{ width: "100%" }} spacing={2}>
       <Alert severity="warning">{props.text}</Alert>
     </Stack>
   );
