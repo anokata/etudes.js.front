bb091f0a9995c13fe290819e5d44d95f4d044486
anokata <anokata8@mail.ru>
2021-10-15 16:53:46 +0300

    [add] my hook 1

--- reactdoc4/src/hooks.js
+++ reactdoc4/src/hooks.js
@@ -4,16 +4,31 @@ import ReactDOM from 'react-dom';
 function State1() {
   const [count, setCount] = useState(0);
   console.log(count, typeof(count));
+  const a = useMyHook("hi");
+
   return (
     <div>
       <p>clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
-        click press this
+        click press this 
+      </button>
+      <button onClick={() => action(count)}>
+        {a}
       </button>
     </div>
   );
 }
 
+let action = () => {};
+
+function useMyHook(str) {
+  const [s, setS] = useState(`[${str}]`);
+  function changeS(x) {
+    setS(`{${x}}${s}`);
+  }
+  action = changeS;
+  return s
+}
 // again
 
 const root = document.getElementById("hook");
