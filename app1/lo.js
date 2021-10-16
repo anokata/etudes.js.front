29dbaf58d4d1f1e30cb9a7c3cf88a5877e9dd2f6
anokata <anokata8@mail.ru>
2021-10-16 18:19:57 +0300

    [add] loadsh

---
+++ app1/lo.js
@@ -0,0 +1,9 @@
+// npm i --save lodash
+var _ = require('lodash');
+// Load the FP build for immutable auto-curried iteratee-first data-last methods.
+var fp = require('lodash/fp');
+// Load method categories.
+var array = require('lodash/array');
+var object = require('lodash/fp/object');
+
+
