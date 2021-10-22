1423af5cd13697090670933c6f587d1f84b29509
anokata <anokata8@mail.ru>
2021-10-22 17:21:29 +0300

    [rxjs] add observer and subsribe

--- app1/rx.mjs
+++ app1/rx.mjs
@@ -1,10 +1,18 @@
 import * as rxjs from 'rxjs';
 const log = console.log;
 // log(rxjs, typeof rxjs);
-const observ1 = new rxjs.Observable(observer =>{ // subscribe fn
+const observable = new rxjs.Observable(observer =>{ // subscribe fn
   observer.next(1)
   observer.next(2)
   observer.next(3)
   observer.complete()
 });
+
+const observer = {
+  next: v => log(`next: ${v}`),
+  error: v => log(`error: ${v}`),
+  complete: () => log(`end`),
+};
+
+observable.subscribe(observer);
 
