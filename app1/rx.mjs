922a111bee933e4e21731364a80b97606d605bd2
anokata <anokata8@mail.ru>
2021-10-22 17:08:59 +0300

    [rxjs] create observer

--- app1/rx.mjs
+++ app1/rx.mjs
@@ -1,4 +1,10 @@
 import * as rxjs from 'rxjs';
 const log = console.log;
 // log(rxjs, typeof rxjs);
+const observ1 = new rxjs.Observable(observer =>{ // subscribe fn
+  observer.next(1)
+  observer.next(2)
+  observer.next(3)
+  observer.complete()
+});
 
