667d8208806e9f8a277544d5f0ed2c2fee9f8b2f
anokata <anokata8@mail.ru>
2021-10-22 17:29:42 +0300

    [rxjs] made timer with unsubscribtion fun

--- app1/rx.mjs
+++ app1/rx.mjs
@@ -1,18 +1,32 @@
-import * as rxjs from 'rxjs';
+import * as rxjs from "rxjs";
 const log = console.log;
 // log(rxjs, typeof rxjs);
-const observable = new rxjs.Observable(observer =>{ // subscribe fn
-  observer.next(1)
-  observer.next(2)
-  observer.next(3)
-  observer.complete()
+const observable = new rxjs.Observable((observer) => {
+  // subscribe fn
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.complete();
 });
 
 const observer = {
-  next: v => log(`next: ${v}`),
-  error: v => log(`error: ${v}`),
+  next: (v) => log(`next: ${v}`),
+  error: (v) => log(`error: ${v}`),
   complete: () => log(`end`),
 };
 
 observable.subscribe(observer);
 
+const timer = new rxjs.Observable((observer) => {
+  let c = 0;
+  const timerID = setInterval(() => {
+    observer.next(c);
+    c++;
+  }, 100);
+
+  return () => clearInterval(timerID);
+});
+
+const subscription = timer.subscribe({ next: log });
+log(`after timer`);
+setTimeout(() => subscription.unsubscribe(), 1000);
