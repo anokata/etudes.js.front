a1d44dd47f1aa3db7cc26d35f21af88fafbc6423
anokata <anokata8@mail.ru>
2021-10-28 17:53:54 +0300

    [add] numpad and atm

--- test-bank/src/app/main.js
+++ test-bank/src/app/main.js
@@ -4,17 +4,18 @@ import { connect } from "react-redux";
 import { doX } from "./store";
 
 export default function Main(props) {
-  return <div>main</div>;
+  return <ATMdispatched />;
 }
 
+const CLEAR_VALUE = "CLR";
 
-
-class A extends React.Component {
+class ATM extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.handleNumpadInput = this.handleNumpadInput.bind(this);
     this.state = {
-      value: "",
+      value: "5432",
     };
   }
 
@@ -24,17 +25,73 @@ class A extends React.Component {
     });
   }
 
+  handleNumpadInput(e) {
+    const num = e.target.value;
+    if (num === CLEAR_VALUE) {
+      this.setState({
+        value: "",
+      });
+    } else {
+      this.setState({
+        value: `${this.state.value}${num}`,
+      });
+    }
+  }
+
   render() {
     return (
-      <input
-        label="Todo text"
-        onChange={(e) => this.handleChange(e)}
-        onKeyDown={(e) => this.handleKeyDown(e)}
-      />
+      <React.Fragment>
+        <input
+          label="Get"
+          type="text"
+          value={this.state.value}
+          onChange={(e) => this.handleChange(e)}
+        />
+        <button>Выдача</button>
+
+        <Numpad onNumpadInput={this.handleNumpadInput}/>
+        <button>Занести</button>
+        <button>Загрузить вариант</button>
+      </React.Fragment>
     );
   }
 }
 
+class Numpad extends React.Component {
+  render() {
+
+    const buttons = Array.from(Array(9).fill(0).keys()).map((i) => 
+      <button 
+        className="numpad-button"
+        onClick={this.props.onNumpadInput}
+        key={i}
+        value={i+1}
+      >{i+1}</button>);
+    return (
+      <div className="numpad">
+        {buttons}
+      <button 
+        className="numpad-button"
+        onClick={this.props.onNumpadInput}
+        key="0"
+        value="0"
+      >0</button>
+      <button 
+        className="numpad-button"
+        onClick={this.props.onNumpadInput}
+        key={","}
+        value={","}
+      >,</button>
+      <button 
+        className="numpad-button"
+        onClick={this.props.onNumpadInput}
+        key={CLEAR_VALUE}
+        value={CLEAR_VALUE}
+      >CLR</button>
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -43,4 +100,4 @@ const mapDispatchToProps = (dispatch) => {
     },
   };
 };
-const Ad = connect(null, mapDispatchToProps)(A);
+const ATMdispatched = connect(null, mapDispatchToProps)(ATM);
