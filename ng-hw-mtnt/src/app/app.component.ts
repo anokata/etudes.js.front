de5897e04e776620a53129994247dee673d19e19
anokata <anokata8@mail.ru>
2021-11-02 09:05:26 +0300

    [ref] move styles and html to files

--- ng-hw-mtnt/src/app/app.component.ts
+++ ng-hw-mtnt/src/app/app.component.ts
@@ -14,66 +14,13 @@ class Item {
 
 @Component({
   selector: "my-app",
-  template: `<div class="page-header">
-      <h1>Список покупок</h1>
-    </div>
-    <div class="panel">
-      <div class="form-inline">
-        <div class="form-group">
-          <div class="col-md-8">
-            <input
-              class="form-control"
-              [(ngModel)]="text"
-              placeholder="Название"
-            />
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-md-6">
-            <input
-              type="number"
-              class="form-control"
-              [(ngModel)]="price"
-              placeholder="Цена"
-            />
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-md-offset-2 col-md-8">
-            <button class="btn btn-default" (click)="addItem(text, price)">
-              Добавить
-            </button>
-          </div>
-        </div>
-      </div>
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th>Предмет</th>
-            <th>Цена</th>
-            <th>Куплено</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr *ngFor="let item of items">
-            <td>{{ item.purchase }}</td>
-            <td>{{ item.price }}</td>
-            <td><input type="checkbox" [(ngModel)]="item.done" /></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>`,
-  styles: [
-    `
-      table {
-        outline: 2px solid #777;
-      }
-    `,
-  ],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.comonent.css"],
 })
 export class AppComponent {
   text: string = "";
   price: number = 0;
+  bindValue = "test bind value";
 
   items: Item[] = [
     { purchase: "Хлеб", done: false, price: 15.9 },
