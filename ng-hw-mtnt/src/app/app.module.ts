a4d80c4e33816fe0d28e1fec730db2a16c5e8257
anokata <anokata8@mail.ru>
2021-11-04 13:14:08 +0300

    [add] test component for event binding in child

--- ng-hw-mtnt/src/app/app.module.ts
+++ ng-hw-mtnt/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
 import { CartList } from './cart-list.component';
+import {  TestComponent } from "./test.component";
 
 @NgModule({
     imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent, CartList ],
+    declarations: [ AppComponent, CartList, TestComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
