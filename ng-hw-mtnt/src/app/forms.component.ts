61b23c62fc886a7fd956aa3126abb2c46739ff8c
anokata <anokata8@mail.ru>
2021-11-05 12:40:05 +0300

    [add] forms component for new page

---
+++ ng-hw-mtnt/src/app/forms.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'forms',
+  templateUrl: 'forms.component.html'
+})
+
+export class FormsComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
