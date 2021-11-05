20066004a0a9cd8f29f23e5a6d5229aa6ac2902b
anokata <anokata8@mail.ru>
2021-11-05 17:06:48 +0300

    [add] GameItem model, form to add and table for view it

--- ng-hw-mtnt/src/app/directives.component.ts
+++ ng-hw-mtnt/src/app/directives.component.ts
@@ -1,20 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+class GameItem {
+  constructor(
+    public name: string,
+    public genre: string,
+    public price: number
+  ) {}
+}
 
 @Component({
-  selector: 'directives',
-  templateUrl: './directives.component.html',
-  styleUrls: ['./directives.component.css']
+  selector: "directives",
+  templateUrl: "./directives.component.html",
+  styleUrls: ["./directives.component.css"],
 })
 export class DirectivesComponent implements OnInit {
   isColoredText: boolean = false;
+
+  name: string = "";
+  genre: string = "";
+  price: number = 0.0;
+
+  games: GameItem[] = [];
+  genres: string[] = ["action", "jrpg", "startegy", "casual"];
+
+  addGameItem() {
+    this.games.push(new GameItem(this.name, this.genre, this.price));
+  }
+
   toggleTextClass() {
     this.isColoredText = !this.isColoredText;
     console.log(this.isColoredText);
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
