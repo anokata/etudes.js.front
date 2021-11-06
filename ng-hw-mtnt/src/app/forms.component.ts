b23d03c585c59b8468c1949981bc6931b2638948
anokata <anokata8@mail.ru>
2021-11-06 12:16:50 +0300

    [add] form element with inputs with basic validation and binding to ngmodel

--- ng-hw-mtnt/src/app/forms.component.ts
+++ ng-hw-mtnt/src/app/forms.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'forms',
-  templateUrl: 'forms.component.html'
+  selector: "forms",
+  templateUrl: "forms.component.html",
+  styleUrls: ["./forms.component.css"],
 })
-
 export class FormsComponent implements OnInit {
-  constructor() { }
+  name: string = "def name";
+  email: string;
+  phone: string;
 
-  ngOnInit() { }
+  submit(form: NgForm) {
+    console.log(form);
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
