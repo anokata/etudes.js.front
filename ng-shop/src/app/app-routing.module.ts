0517004e8121d9e749a3d47fbef83ad61d9d9c35
anokata <anokata8@mail.ru>
2021-11-07 18:19:12 +0300

    [add] routing

--- ng-shop/src/app/app-routing.module.ts
+++ ng-shop/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { MainLayoutComponent } from './shared/main-layout.component';
+import { MainPageComponent } from './shared/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: '', component: MainPageComponent },
+      { path: 'product/:id', component: ProductPageComponent },
+      { path: 'cart', component: CartPageComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
