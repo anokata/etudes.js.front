7711e6202b27e40d30d933324aa987f9cffd0074
anokata <anokata8@mail.ru>
2021-11-09 09:47:00 +0300

    [add] auth guard and use its to safe admin routes

--- ng-shop/src/app/admin/admin.module.ts
+++ ng-shop/src/app/admin/admin.module.ts
@@ -8,6 +8,7 @@ import { AddPageComponent } from './add-page/add-page.component';
 import { OrdersPageComponent } from './orders-page/orders-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthGuard } from '../shared/auth.guard';
 
 @NgModule({
   declarations: [
@@ -29,10 +30,26 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
         children: [
           { path: '', redirectTo: '/admin/login', pathMatch: 'full' },
           { path: 'login', component: LoginPageComponent },
-          { path: 'dashboard', component: DashboardPageComponent },
-          { path: 'add', component: AddPageComponent },
-          { path: 'orders', component: OrdersPageComponent },
-          { path: 'product/:id/edit', component: EditPageComponent },
+          {
+            path: 'dashboard',
+            component: DashboardPageComponent,
+            canActivate: [AuthGuard],
+          },
+          {
+            path: 'add',
+            component: AddPageComponent,
+            canActivate: [AuthGuard],
+          },
+          {
+            path: 'orders',
+            component: OrdersPageComponent,
+            canActivate: [AuthGuard],
+          },
+          {
+            path: 'product/:id/edit',
+            component: EditPageComponent,
+            canActivate: [AuthGuard],
+          },
         ],
       },
     ]),
