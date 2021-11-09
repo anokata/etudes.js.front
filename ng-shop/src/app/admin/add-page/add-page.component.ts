8a7244d8632c8916d909179ac0ada01995bfeaff
anokata <anokata8@mail.ru>
2021-11-09 14:45:17 +0300

    [add] basic form controls for add product page

--- ng-shop/src/app/admin/add-page/add-page.component.ts
+++ ng-shop/src/app/admin/add-page/add-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-page',
   templateUrl: './add-page.component.html',
-  styleUrls: ['./add-page.component.scss']
+  styleUrls: ['./add-page.component.scss'],
 })
 export class AddPageComponent implements OnInit {
+  form: FormGroup;
+  submitted: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      type: new FormControl(null, Validators.required),
+      title: new FormControl(null, Validators.required),
+      photo: new FormControl(null, Validators.required),
+      info: new FormControl(null, Validators.required),
+      price: new FormControl(null, Validators.required),
+    });
   }
 
+  submit() {}
 }
