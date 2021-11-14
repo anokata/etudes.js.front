886c9c0f2ed9c3050f5a2d657ef9bfbeb57576ac
anokata <anokata8@mail.ru>
2021-11-14 11:21:59 +0300

    [add] Categories component, data service, extract aside

--- ng-todo/src/app/app.module.ts
+++ ng-todo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CategoriesComponent } from './views/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
