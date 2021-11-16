f8c2937b64dbb7178cb7f842c13f7d1689b691c3
anokata <anokata8@mail.ru>
2021-11-16 20:40:26 +0300

    [chg] gulpfile

--- js-gulp-html/gulpfile.js
+++ js-gulp-html/gulpfile.js
@@ -17,4 +17,26 @@ const del = require("del");
 const panini = require("panini");
 const browsersync = require("browser-sync").create();
 
+let path = {
+  build: {
+    html: "dist/",
+    js: "dist/assets/js",
+    css: "dist/assets/css",
+    images: "dist/assets/img"
+  },
+  src: {
+    html: "src/*.html",
+    js: "src/assets/js/*.js",
+    css: "src/assets/sass/style.scss",
+    images: "src/assets/img/**/*.{jpg,png,svg,gif,jpeg,ico}"
+  },
+  watch: {
+    html: "src/**/*.html",
+    js: "src/assets/js/**/*.js",
+    css: "src/assets/sass/**/*.scss",
+    images: "src/assets/img/**/*.{jpg,png,svg,gif,jpeg,ico}"
+  },
+  clean: "./dist"
+}
+
 
