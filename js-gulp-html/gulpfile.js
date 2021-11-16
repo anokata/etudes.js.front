a474ec3f5e86e334ac69adbefaec87bde2be040b
anokata <anokata8@mail.ru>
2021-11-16 21:35:20 +0300

    [chg] gulp file, tasks - build all, watch

--- js-gulp-html/gulpfile.js
+++ js-gulp-html/gulpfile.js
@@ -40,3 +40,90 @@ let path = {
 }
 
 
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./dist/"
+    },
+    port: 3000
+  });
+}
+
+function browserSyncReload(done) {
+  browsersync.reload();
+}
+
+function html() {
+  return src(path.src.html, { base: "src/" })
+    .pipe(plumber())
+    .pipe(dest(path.build.html))
+}
+
+function css() {
+  return src(path.src.css, { base: "src/assets/sass" })
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      Browserslist: ['last 5 versions'],
+      cascade: true
+    }))
+    .pipe(cssbeautify())
+    .pipe(dest(path.build.css))
+    .pipe(cssnano({
+      zindex: false,
+      discardComments: {
+        removeAll: true
+      }
+    }))
+    .pipe(removeComments())
+    .pipe(rename({
+      suffix: ".min",
+      extname: ".css"
+    }))
+    .pipe(dest(path.build.css))
+
+}
+
+function js() {
+  return src(path.src.js, { base: './src/assets/js/' })
+    .pipe(plumber())
+    .pipe(rigger())
+    .pipe(gulp.dest(path.build.js))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min",
+      extname: ".js"
+    }))
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream());
+}
+
+function images() {
+  return src(path.src.images)
+    .pipe(imagemin())
+    .pipe(dest(path.build.images));
+}
+
+function clean() {
+  return del(path.clean);
+}
+
+function watchFiles() {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.images], images);
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, js, images));
+const watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.images = images;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
+
