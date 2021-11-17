0007f535b18c265fef626ba8a4bb5b75e56b59fa
anokata <anokata8@mail.ru>
2021-11-17 15:39:11 +0300

    [webpack] html plugin, dev server [conf] eslint, pkg

--- js-webpack/webpack.config.js
+++ js-webpack/webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  module: {
+    // rules: [{ test: /\.js$/, use: 'raw-loader' }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/pages/index.html' })],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
   },
 };
