2654bb685ce5807f773f2759653ab9b0c00d656a
anokata <anokata8@mail.ru>
2021-11-18 10:18:43 +0300

    [add] bootstrap

--- js-gulp-html/src/assets/js/app.js
+++ js-gulp-html/src/assets/js/app.js
@@ -1,3 +1,4 @@
 //= components/tabs.js
 //= components/slider.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
 console.log(1);
--- js-gulp-html/src/assets/js/components/slider.js
+++ js-gulp-html/src/assets/js/components/slider.js
@@ -1,31 +1,37 @@
 //= ../../../../node_modules/slick-carousel/slick/slick.js
 
-document.querySelector(".slider").slick({
-
-  // normal options...
-  infinite: false,
-
-  // the magic
-  responsive: [{
-
-    breakpoint: 1024,
-    settings: {
-      slidesToShow: 3,
-      infinite: true
-    }
-
-  }, {
-
-    breakpoint: 600,
-    settings: {
-      slidesToShow: 2,
-      dots: true
-    }
-
-  }, {
-
-    breakpoint: 300,
-    settings: "unslick" // destroys slick
-
-  }]
+document.addEventListener("load", () => {
+  mainLoad();
 });
+
+function mainLoad() {
+  document.querySelector(".slider").slick({
+
+    // normal options...
+    infinite: false,
+
+    // the magic
+    responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+  });
+}
