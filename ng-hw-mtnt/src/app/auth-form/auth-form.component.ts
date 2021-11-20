d36e7bcadd107454cb6f5c2904d51c68b284d483
anokata <anokata8@mail.ru>
2021-11-20 11:10:49 +0300

    [chg] auth form basic template and form

--- ng-hw-mtnt/src/app/auth-form/auth-form.component.ts
+++ ng-hw-mtnt/src/app/auth-form/auth-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'auth-form',
-  templateUrl: './auth-form.component.html',
-  styleUrls: ['./auth-form.component.css']
+  selector: "auth-form",
+  templateUrl: "./auth-form.component.html",
+  styleUrls: ["./auth-form.component.css"],
 })
 export class AuthFormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.form = new FormGroup({
+      username: new FormControl("default", [Validators.required]),
+      password: new FormControl("123", [Validators.required]),
+    });
   }
 
+  ngOnInit(): void {}
+
+  login(): void {
+    console.log(this.form.value);
+  }
 }
