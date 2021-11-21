6a7031f8c53bf9b12bc2066a7129d9cda66ccbfb
anokata <anokata8@mail.ru>
2021-11-21 09:51:25 +0300

    [add] firebase

--- ng-hw-mtnt/src/main.ts
+++ ng-hw-mtnt/src/main.ts
@@ -1,5 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+import { initializeApp } from "firebase/app";
+import { environment } from "./environments/environment";
 
+const app = initializeApp(environment.firebase);
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
